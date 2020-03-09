203a5026e463ffb639825f2fdb29e50e928dd720
Randy Daddis <randy@daddis.tech>
2020-03-09 13:39:42 -0400

    add global constants

---
+++ s/spa/src/constants/labels.ts
@@ -0,0 +1,52 @@
+export const labels = {
+
+  accounting: 'Accounting',
+  contactUs: 'Contact Us',
+  realEstate: 'Real Estate',
+  service: 'Service',
+  software: 'Software',
+  technology: 'Technology',
+  welcome: 'Welcome',
+
+  // button labels
+  add: 'Add',
+  delete: 'Delete',
+  edit: 'Edit',
+  cancel: 'Cancel',
+  login: 'Login',
+  logout: 'Logout',
+  readMore: 'Read More',
+  register: 'register',
+  save: 'Save',
+
+  // input labels
+  email: 'E-mail',
+  firstName: 'First Name',
+  is: ' is ',
+  lastName: 'Last name',
+  fullName: 'Full Name',
+  password: 'Password',
+  passwordConfirmation: 'Password Confirmation',
+  phoneNumber: 'Phone Number',
+  url: 'URL',
+  userId: 'User ID',
+  userName: 'Username',
+  website: 'website',
+
+  // miscellaneous labels
+  and: 'and',
+  characters: 'characters',
+  existingUser: 'Already have an account?',
+  invalid: 'invalid',
+  loginFailed: 'login failed',
+  match: 'match',
+  must: 'must',
+  mustBeBetween: ' must be between ',
+  mustBeGreaterThanOrEqual: ' must be >= ',
+  mustBeLessThanOrEqual: ' must be <= ',
+  notRegistered: 'Not a member?',
+  required: 'required',
+  registrationFailed: 'registration failed',
+  registrationInvitationCode: 'Registration Invitation Code',
+  socialLogin: 'or log in with:'
+};
---
+++ s/spa/src/constants/website-metadata.ts
@@ -0,0 +1,15 @@
+const year = new Date().getFullYear();
+
+export const websiteMetadata = {
+
+  name: 'Daddis Consulting Services, LLC',
+  addressline1: '279 Raymondskill Rd',
+  city: 'Milford',
+  stateOrProvince: 'PA',
+  postalCode: '18337',
+  phoneNumber: '(862) 777 - 4443',
+  email: 'info@daddis.tech',
+  url: 'daddis.tech', // TODO: refactor name to url
+  copyright: 'Copyright ' + year + ' Daddis Consulting Services, LLC',
+  googleAnalyticsId: 'UA-9898164-2'
+};
