a37b8d81c01fbc66f3341c91ffc0c0d13c854afa
Randy Daddis <randy@daddis.tech>
2020-03-09 14:49:03 -0400

    add routes

--- s/spa/src/app/app-routing.module.ts
+++ s/spa/src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ContactUsComponent } from 'src/app/core/components/public/contact-us/contact-us.component';
+import { HomeComponent } from 'src/app/core/components/public/home/home.component';
+import { ServicesComponent } from 'src/app/core/components/public/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: '',
+    children: [
+      {
+        path: 'contact-us',
+        component: ContactUsComponent
+      },
+      {
+        path: 'services',
+        component: ServicesComponent
+      }
+    ]
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    { enableTracing: true } /*debugging purposes only*/)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
